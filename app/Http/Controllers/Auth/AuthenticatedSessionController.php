7fd7b48932be47c806655631e0a7e654f5c561f0
ShivenderPandey <56041335+ShivenderPandey@users.noreply.github.com>
2026-01-10 15:46:09 +0000

    fix login direction

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -23,19 +23,15 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    // public function store(LoginRequest $request): RedirectResponse
-    // {
-    //     $request->authenticate();
+     public function store(LoginRequest $request): RedirectResponse
+     {
+         $request->authenticate();
 
-    //     $request->session()->regenerate();
+         $request->session()->regenerate();
 
-    //     return redirect()->intended(RouteServiceProvider::Home)->('success', 'Welcome! You have logged in successfully');
-    // }
-    public function store(LoginRequest $request): RedirectResponse 
-    {
-    return redirect()->intended(RouteServiceProvider::HOME)
-    ->with('success', 'Welcome! You have logged in successfully.');
-    }
+         return redirect()->intended(route('dashboard', absolute: false))
+         ->with('success', 'Welcome! You have logged in successfully');
+     }
 
     /**
      * Destroy an authenticated session.
